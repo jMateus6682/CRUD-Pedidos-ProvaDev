5b0fa88ebe6a48467c1e94f54e3bf9387505c3dd
Joao Mateus <51335680+jMateus6682@users.noreply.github.com>
2022-03-18 16:19:00 -0400

    adicionando dnv uma parte que deletei errado

--- CRUD_pedidos/CRUD_MVC5_AngularJs/Controllers/HomeController.cs
+++ CRUD_pedidos/CRUD_MVC5_AngularJs/Controllers/HomeController.cs
@@ -11,6 +11,20 @@ namespace CRUD_MVC5_AngularJs.Controllers
         public ActionResult Index()
         {
             return View();
-        }     
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
     }
 }
